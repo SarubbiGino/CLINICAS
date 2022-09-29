b8b17efa4db04ba5d2046035a70b1a0c7039a495
Joaquin Blanco <89130929+jblancoetec@users.noreply.github.com>
2022-09-29 12:33:38 -0300

    delete.next

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/infoAdmins/infoAdm":["static\u002Fchunks\u002Fpages\u002FinfoAdmins\u002FinfoAdm.js"],sortedPages:["\u002F_app","\u002F_error","\u002FinfoAdmins\u002FinfoAdm"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
