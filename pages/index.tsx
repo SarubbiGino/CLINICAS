1d2aa54e0d642512aad4ada0950c3199a503aad5
Joaquin Blanco <blancojoaquin77@gmail.com>
2022-10-07 17:51:22 -0300

    add admin ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,19 @@
 import styles from "../styles/Home.module.css";
-import Layout from "../src/layout/layout";
 import TituloMain from "../src/components/tituloMain/tituloMain";
-import TarjetaMain from "../src/components/tarjetasMain/tarjetasMain";
-import Button from "@mui/material/Button";
 import Tarjetas from "../src/components/tarjetasMain/taerjeta-id";
-import imagen1 from "../img/donadores.jpg";
-import { Box } from "@mui/material";
-import Footer from "../src/components/Footer";
 import { Container } from "@mui/system";
-import { Provider } from "react-redux";
-import { store } from "../src/app/store";
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <Provider store={store}>
     <Container>
       <main className={styles.main}>
         <TituloMain />
         <h1 className={styles.empiezaDonar}>Empieza a Donar</h1>
-        <h2 >¿COMO DONAR SANGRE?</h2>
-        <Tarjetas/>
+        <h2>¿COMO DONAR SANGRE?</h2>
+        <Tarjetas />
       </main>
-   
     </Container>
-    </Provider>
   );
 };
 
--- pages/infoAdmins/infoAdm.tsx
+++ pages/infoAdmins/infoAdm.tsx
@@ -1,58 +1,44 @@
-import React, {useState} from 'react';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import { getMaxListeners } from 'process';
-import Button from '@mui/material/Button';
-import estilos from '../infoAdmins/info.module.css'
-import AddIcon from '../infoAdmins/modalAdd'
-import { ClassNames } from '@emotion/react';
-import { deleteAdmin } from "../../src/features/admin/adminSlice";
+import React from "react";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import AddIcon from "../infoAdmins/modalAdd";
 
-import type { RootState, AppDispatch } from '../../src/app/store'
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+import type { RootState, AppDispatch } from "../../src/app/store";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 20 },
-  { field: 'first_name', headerName: 'Nombre', width: 110 },
-  { field: 'last_name', headerName: 'Apellido', width: 120 },
-  { field: 'email', headerName: 'Mail', width: 190 },
+  { field: "id", headerName: "ID", width: 20 },
+  { field: "first_name", headerName: "Nombre", width: 110 },
+  { field: "last_name", headerName: "Apellido", width: 120 },
+  { field: "email", headerName: "Mail", width: 190 },
 
   {
-    field: 'fullName',
-    headerName: 'Nombre Completo',
-    description: 'This column has a value getter and is not sortable.',
+    field: "fullName",
+    headerName: "Nombre Completo",
+    description: "This column has a value getter and is not sortable.",
     sortable: false,
     width: 160,
     valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.first_name || ''} ${params.row.last_name || ''}`,
+      `${params.row.first_name || ""} ${params.row.last_name || ""}`,
   },
-]; 
+];
 
 export default function DataTable() {
-  const handleDelete = (id) => {
-    dispatch(deleteAdmin(id));
-  };
-  
-    const admins = useAppSelector ((state) => state.admins)
+  const admins = useAppSelector((state) => state.admins);
 
   return (
     <React.Fragment>
       <p>Lista de administradores</p>
       <div style={{ height: 500 }}>
-      <DataGrid
-        rows={admins}
-        columns={columns}
-        pageSize={15}
-        rowsPerPageOptions={[15]}
-      />
-    </div>
-     <button
-     onClick={() => handleDelete(admin.id)}>
-     delete
-     </button>
-    <AddIcon/>
+        <DataGrid
+          rows={admins}
+          columns={columns}
+          pageSize={15}
+          rowsPerPageOptions={[15]}
+        />
+      </div>
+      <AddIcon />
     </React.Fragment>
-    
   );
 }
